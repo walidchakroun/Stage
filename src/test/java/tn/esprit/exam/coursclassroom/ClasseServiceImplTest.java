7e9d9000bdf5f92038ac6ffabcd4dca68d2779f2
Walidchakroun <Walid.chakroun@esprit.tn>
2025-08-20 13:32:31 +0100

    Add first JUnit test for ClasseServiceImpl

---
+++ src/test/java/tn/esprit/exam/coursclassroom/ClasseServiceImplTest.java
@@ -0,0 +1,12 @@
+package tn.esprit.exam.coursclassroom;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClasseServiceImplTest {
+
+    @Test
+    void dummyTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
